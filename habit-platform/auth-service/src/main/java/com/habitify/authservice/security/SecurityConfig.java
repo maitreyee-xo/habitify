9d4c7707580a4525bf56778de1a5b567280548a5
maitreyee-xo <maitreyees20@gmail.com>
2025-12-14 22:20:39 +0530

    Working as expected for user login/verification

--- habit-platform/auth-service/src/main/java/com/habitify/authservice/security/SecurityConfig.java
+++ habit-platform/auth-service/src/main/java/com/habitify/authservice/security/SecurityConfig.java
@@ -3,29 +3,15 @@ package com.habitify.authservice.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-            .csrf(ServerHttpSecurity.CsrfSpec::disable)
-            .authorizeExchange(exchange -> exchange
-                .pathMatchers("/auth/**").permitAll()
-                .anyExchange().permitAll() // TEMPORARY
-            )
-            .build();
-    }
     private final JwtAuthFilter jwtAuthFilter;
 
     public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
